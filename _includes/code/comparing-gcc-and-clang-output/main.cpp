b40cbfada325238e28934373b38d952297afa873
Bernardo Sulzbach <mafagafogigante@gmail.com>
2018-01-16 10:56:40 -0200

    Should perform more iterations on the comparison

--- _includes/code/comparing-gcc-and-clang-output/main.cpp
+++ _includes/code/comparing-gcc-and-clang-output/main.cpp
@@ -30,20 +30,16 @@ float evaluateGCC(float a, float b) {
 
 int main() {
   auto start = std::chrono::steady_clock::now();
-  for (int j = 0; j < 1000; j++) {
-    for (int i = 0; i < 65536; i++) {
-      evaluateClang(4.0f, 9.0f);
-    }
+  for (int j = 0; j < 1000 * 1000 * 1000; j++) {
+    evaluateClang(4.0f, 9.0f);
   }
   auto now = std::chrono::steady_clock::now();
   std::chrono::duration<double, std::nano> ns = now - start;
   cout << ns.count() << " ns" << '\n';
 
   start = std::chrono::steady_clock::now();
-  for (int j = 0; j < 1000; j++) {
-    for (int i = 0; i < 65536; i++) {
-      evaluateGCC(4.0f, 9.0f);
-    }
+  for (int j = 0; j < 1000 * 1000 * 1000; j++) {
+    evaluateGCC(4.0f, 9.0f);
   }
   now = std::chrono::steady_clock::now();
   ns = now - start;
