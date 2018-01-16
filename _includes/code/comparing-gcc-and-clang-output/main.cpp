451684733418b228662297cedbcf69fb4774c380
Bernardo Sulzbach <mafagafogigante@gmail.com>
2018-01-16 10:41:02 -0200

    Should use a single asm statement per function
    GCC can reorder multiple asm statements.

--- _includes/code/comparing-gcc-and-clang-output/main.cpp
+++ _includes/code/comparing-gcc-and-clang-output/main.cpp
@@ -4,28 +4,28 @@
 
 using namespace std;
 
-asm(".LC3:");
-asm("  .long 1065353216");
-asm(".LC4:");
-asm("  .long 3212836864");
+asm(".LC3:\n"
+    "  .long 1065353216");
+asm(".LC4:\n"
+    "  .long 3212836864");
 
 float evaluateClang(float a, float b) {
-  asm("subss xmm0, xmm1");
-  asm("movss xmm1, dword ptr [rip + .LC3]");
-  asm("addss xmm1, xmm0");
-  asm("mulss xmm1, xmm0");
-  asm("addss xmm0, dword ptr [rip + .LC4]");
-  asm("mulss xmm0, xmm1");
+  asm("subss xmm0, xmm1\n"
+      "movss xmm1, dword ptr [rip + .LC3]\n"
+      "addss xmm1, xmm0\n"
+      "mulss xmm1, xmm0\n"
+      "addss xmm0, dword ptr [rip + .LC4]\n"
+      "mulss xmm0, xmm1");
 }
 
 float evaluateGCC(float a, float b) {
-  asm("subss xmm0, xmm1");
-  asm("movss xmm2, dword ptr [rip + .LC3]");
-  asm("movaps xmm1, xmm0");
-  asm("addss xmm1, xmm2");
-  asm("mulss xmm1, xmm0");
-  asm("subss xmm0, xmm2");
-  asm("mulss xmm0, xmm1");
+  asm("subss xmm0, xmm1\n"
+      "movss xmm2, dword ptr [rip + .LC3]\n"
+      "movaps xmm1, xmm0\n"
+      "addss xmm1, xmm2\n"
+      "mulss xmm1, xmm0\n"
+      "subss xmm0, xmm2\n"
+      "mulss xmm0, xmm1");
 }
 
 int main() {
